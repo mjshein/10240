944d698223b691eca4d72977c536ae32789c14ed
mjshein <59715207+mjshein@users.noreply.github.com>
2020-01-30 17:28:37 -0500

    fixed mass start tiles

--- js/game_manager.js
+++ js/game_manager.js
@@ -60,7 +60,7 @@ GameManager.prototype.setup = function () {
 
 // Set up the initial tiles to start the game with
 GameManager.prototype.addStartTiles = function () {
-  for (var i = 0; i < this.startTiles; i++) {
+  for (var i = 8; i < this.startTiles; i++) {
     this.addRandomTile();
   }
 };
